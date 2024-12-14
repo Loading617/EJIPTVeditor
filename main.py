d1ea5e6ab8257e7daa511a2b56881a828948c628
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-14 06:39:57 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import customtkinter as ctk
 
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("redorange")
-
 root = ctk.CTk()
 root.title("EJ IPTV Editor")
 
-root.geometry("1280x720")
+root.geometry("600x240")
 
 root.mainloop()

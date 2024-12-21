3e15ce86334df600cca41b5a640184ba7615a4f3
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-21 16:44:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,37 +2,31 @@ import customtkinter as ctk
 from CTkMenuBar import CTkMenuBar
 
 root = ctk.CTk()
-root.title("EJ IPTV editor")
+root.title("EJ IPTV Editor")
 
 menubar = CTkMenuBar(root)
 menubar.pack(side="top", fill="x")
 
-file_menu = menubar.add_cascade("File")
+file_menu = menubar.add_menu("File")
 file_menu.add_command(label="New List", command=lambda: print("New"))
 file_menu.add_command(label="Open List", command=lambda: print("Open"))
-file_menu.add_command(label="Open URL", command=lambda: print("Open"))
+file_menu.add_command(label="Open URL", command=lambda: print("Open URL"))
 file_menu.add_separator()
-file_menu.add_command(label="Exit", command=root.quit, print("Exit"))
+file_menu.add_command(label="Exit", command=lambda: (print("Exit"), root.quit()))
 
-import_menu = menubar.add_cascade("Import")
-import_menu.add_command(label="Import Kodi IPTV List", command=lambda: print("Import"))
-import_menu.add_command(label="Import Simple IPTV List", command=lambda: print("Import"))
+import_menu = menubar.add_menu("Import")
+import_menu.add_command(label="Import Kodi IPTV List", command=lambda: print("Import Kodi"))
+import_menu.add_command(label="Import Simple IPTV List", command=lambda: print("Import Simple"))
 
-export_menu = menubar.add_cascade("Export")
-export_menu.add_command(label="Export Kodi IPTV List", command=lambda: print("Export"))
-export_menu.add_command(label="Export Simple IPTV List", command=lambda: print("Export"))
+export_menu = menubar.add_menu("Export")
+export_menu.add_command(label="Export Kodi IPTV List", command=lambda: print("Export Kodi"))
+export_menu.add_command(label="Export Simple IPTV List", command=lambda: print("Export Simple"))
 
-about_menu = menubar.add_cascade("About")
+about_menu = menubar.add_menu("About")
 about_menu.add_command(label="About EJ IPTV Editor", command=lambda: print("About"))
 
-help_menu = menubar.add_cascade("Help")
+help_menu = menubar.add_menu("Help")
 help_menu.add_command(label="How to", command=lambda: print("How to"))
-
-menubar.add_cascade(label="File", menu=file_menu)
-menubar.add_cascade(label="Import", menu=import_menu)
-menubar.add_cascade(label="Export", menu=export_menu)
-menubar.add_cascade(label="About", menu=about_menu)
-menubar.add_cascade(label="Help", menu=help_menu)
 
 root.geometry("920x570")
 

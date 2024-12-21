033dbb08b9a98a814b9fc97c2d90d806e39d8cc4
Jahim Sankoh <jahimsankoh319@gmail.com>
2024-12-21 17:02:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,26 +7,32 @@ root.title("EJ IPTV Editor")
 menubar = CTkMenuBar(root)
 menubar.pack(side="top", fill="x")
 
-file_menu = menubar.add_menu("File")
+file_menu = ctk.CTkMenu(root)
 file_menu.add_command(label="New List", command=lambda: print("New"))
 file_menu.add_command(label="Open List", command=lambda: print("Open"))
 file_menu.add_command(label="Open URL", command=lambda: print("Open URL"))
 file_menu.add_separator()
 file_menu.add_command(label="Exit", command=lambda: (print("Exit"), root.quit()))
 
-import_menu = menubar.add_menu("Import")
+menubar.add_cascade(label="File", menu=file_menu)
+
+import_menu = ctk.CTkMenu(root)
 import_menu.add_command(label="Import Kodi IPTV List", command=lambda: print("Import Kodi"))
 import_menu.add_command(label="Import Simple IPTV List", command=lambda: print("Import Simple"))
+menubar.add_cascade(label="Import", menu=import_menu)
 
-export_menu = menubar.add_menu("Export")
+export_menu = ctk.CTkMenu(root)
 export_menu.add_command(label="Export Kodi IPTV List", command=lambda: print("Export Kodi"))
 export_menu.add_command(label="Export Simple IPTV List", command=lambda: print("Export Simple"))
+menubar.add_cascade(label="Export", menu=export_menu)
 
-about_menu = menubar.add_menu("About")
+about_menu = ctk.CTkMenu(root)
 about_menu.add_command(label="About EJ IPTV Editor", command=lambda: print("About"))
+menubar.add_cascade(label="About", menu=about_menu)
 
-help_menu = menubar.add_menu("Help")
+help_menu = ctk.CTkMenu(root)
 help_menu.add_command(label="How to", command=lambda: print("How to"))
+menubar.add_cascade(label="Help", menu=help_menu)
 
 root.geometry("920x570")
 
